1ccd95e055060b792ae0a7abf9aebb464288635e
Irhas <irhasm@outlook.com>
2025-09-06 23:03:56 +0700

    fix(page): remove console log

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,8 +38,11 @@
   const obj: any = data as any,
     posts: App.Post[] = obj.posts as App.Post[],
     mobileApps: App.MobileApp[] = obj.apps as App.MobileApp[]
+<<<<<<< HEAD
 
   console.log(mobileApps)
+=======
+>>>>>>> 3f8b22b (fix(page): remove console log)
 </script>
 
 <MetaHome />
